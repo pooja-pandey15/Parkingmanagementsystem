8407c45964898d76b98ada3b62ee97d2a8cfb05b
pooja-pandey15 <poojapandeypp031@gmail.com>
2025-02-20 15:52:18 +0545

    fixed park and leave

--- models.py
+++ models.py
@@ -1,15 +1,14 @@
-from datetime import datetime
+from db import add_vehicle, exit_vehicle
 
 class ParkedVehicle:
-    def __init__(self, vehicle_type, plate_number, entry_time, exit_time=None):
-        self.vehicle_type = vehicle_type
+    def __init__(self, plate_number, vehicle_type):
         self.plate_number = plate_number
-        self.entry_time = datetime.strptime(entry_time, "%Y-%m-%d %H:%M:%S")
-        self.exit_time = datetime.strptime(exit_time, "%Y-%m-%d %H:%M:%S") if exit_time else None
+        self.vehicle_type = vehicle_type
 
-    def calculate_fare(self):
-        if not self.exit_time:
-            return 0.0
-        duration = (self.exit_time - self.entry_time).total_seconds() / 3600
-        rate = 50 if self.vehicle_type.lower() == "car" else 20
-        return round(duration * rate, 2)
+    def park(self):
+        """Saves vehicle entry in the database."""
+        return add_vehicle(self.plate_number, self.vehicle_type)
+
+    def leave(self):
+        """Processes vehicle exit and calculates fare."""
+        return exit_vehicle(self.plate_number)
