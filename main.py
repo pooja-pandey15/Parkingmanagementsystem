069d31cd7713a7722394fd4a78620d334fcade38
pooja-pandey15 <poojapandeypp031@gmail.com>
2025-02-20 15:56:11 +0545

    Upated main file

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-import ui
+from db import initialize_db
+from ui import ParkingApp
+import tkinter as tk
+
+if __name__ == "__main__":
+    initialize_db()  # Ensure database setup
+    root = tk.Tk()
+    app = ParkingApp(root)
+    root.mainloop()
